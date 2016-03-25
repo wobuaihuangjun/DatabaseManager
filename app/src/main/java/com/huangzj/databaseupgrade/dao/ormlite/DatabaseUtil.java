aa2d3eaba446d019a377e2e3f8eeb7f805975ebf
wobuaihuangjun <892105817@qq.com>
2016-03-25 10:06:55 +0800

    重构代码

--- app/src/main/java/com/huangzj/databaseupgrade/dao/ormlite/DatabaseUtil.java
+++ app/src/main/java/com/huangzj/databaseupgrade/dao/ormlite/DatabaseUtil.java
@@ -17,15 +17,16 @@ import java.util.List;
 import timber.log.Timber;
 
 /**
+ * 数据库处理的辅助工具
+ * <p/>
  * Created by huangzj on 2016/1/23.
  */
 public class DatabaseUtil {
 
-
     /**
      * 获取表名
      *
-     * @param clazz
+     * @param clazz 表的实体bean
      * @param <T>
      * @return
      */
@@ -43,11 +44,10 @@ public class DatabaseUtil {
         return name;
     }
 
-
     /**
      * 获取数据库中对应表的列名集合
      *
-     * @param columnStructList
+     * @param columnStructList 表的列结构集合
      * @return
      */
     public static List<String> getColumnNames(List<ColumnStruct> columnStructList) {
@@ -66,9 +66,9 @@ public class DatabaseUtil {
      * 生成新的数据表结构
      *
      * @param connectionSource
-     * @param clazz
+     * @param clazz            表的实体bean
      * @param <T>
-     * @return
+     * @return 新版本数据库表的列结构集合
      */
     public static <T> List<ColumnStruct> getNewTableStruct(ConnectionSource connectionSource, Class<T> clazz) {
         List<ColumnStruct> columnStruct = new ArrayList<>();
@@ -77,8 +77,7 @@ public class DatabaseUtil {
             Timber.i("新的建表语句：" + struct);
             columnStruct = getColumnStruct(struct);
         } catch (SQLException e) {
-            Timber.e("", e);
-            ;
+            Timber.e("",e);
         }
         return columnStruct;
     }
@@ -87,11 +86,10 @@ public class DatabaseUtil {
      * 查询数据库中对应表的表结构
      *
      * @param db
-     * @param tableName
-     * @return
+     * @param tableName 表名
+     * @return 旧版本数据库表的列结构集合
      */
     public static List<ColumnStruct> getOldTableStruct(SQLiteDatabase db, String tableName) {
-
         List<ColumnStruct> columnStruct = new ArrayList<>();
         Cursor cursor = null;
         try {
@@ -104,7 +102,11 @@ public class DatabaseUtil {
                     String struct = cursor.getString(columnIndex);
                     Timber.i("旧的建表语句：" + struct);
                     columnStruct = getColumnStruct(struct);
+                } else {
+                    Timber.i("不存在旧表");
                 }
+            } else {
+                Timber.i("数据库操作失败");
             }
         } catch (Exception e) {
             Timber.e("", e);
@@ -117,13 +119,12 @@ public class DatabaseUtil {
     }
 
     /**
-     * 生成表结构
+     * 生成表结构集合
      *
      * @param tableStruct 规范建表语句
-     * @return
+     * @return 表结构集合
      */
     public static List<ColumnStruct> getColumnStruct(String tableStruct) {
-        // 这个函数总是给人一种不爽的感觉，欢迎提出更优秀的方案
         List<ColumnStruct> columnStructList = new ArrayList<>();
 //      "CREATE TABLE `msg_record` (`id` INTEGER PRIMARY KEY AUTOINCREMENT , `title` VARCHAR ,  UNIQUE (`number`))"
         // 解析过程根据标准的ormlite建表语句设计
@@ -133,7 +134,7 @@ public class DatabaseUtil {
             if (str.contains("(") || str.contains(")")) {
                 str = str.replace("(", "").replace(")", "");
             }
-            str = removeUnlessSpace(str);
+            str = str.trim();//去除字符串的首尾空格
             if (str.startsWith("`")) {
                 String[] column = str.split("` ");
                 columnStructList.add(new ColumnStruct(column[0].replace("`", ""), column[1]));
@@ -154,13 +155,24 @@ public class DatabaseUtil {
         return columnStructList;
     }
 
+    /**
+     * 修改列结构
+     * 部分列结构有多个限制字段，需要进行合并
+     *
+     * @param list       需要加入的列结构集合
+     * @param columnName 需要合并的列名
+     * @param limit      额外限制字段
+     */
     private static void modifyColumnStruct(List<ColumnStruct> list, String columnName, String limit) {
         if (list == null || list.isEmpty()) {
+            Timber.e("list is null.");
             return;
         }
         if (TextUtils.isEmpty(columnName) || TextUtils.isEmpty(limit)) {
+            Timber.e("columnName is null or limit is null.");
             return;
         }
+
         int size = list.size();
         for (int i = 0; i < size; i++) {
             ColumnStruct struct = list.get(i);
@@ -177,58 +189,59 @@ public class DatabaseUtil {
     }
 
     /**
-     * 去除首尾空格
-     */
-    private static String removeUnlessSpace(String str) {
-        while (str.startsWith(" ")) {
-            str = str.substring(1);
-        }
-        while (str.endsWith(" ")) {
-            str = str.substring(0, str.length() - 1);
-        }
-        return str;
-    }
-
-    /**
-     * 判断数据表已有字段的描述是否有改变
+     * 判断数据表已有字段的限制是否有改变
      *
-     * @param oldStructList
-     * @param newStructList
-     * @return
+     * @param oldStructList 旧的表列结构集合
+     * @param newStructList 新的表列结构集合
+     * @return true：改变；false：未改变
      */
     public static boolean hasChangeColumnLimit(List<ColumnStruct> oldStructList,
                                                List<ColumnStruct> newStructList) {
         if (oldStructList == null || newStructList == null) {
             return false;
         }
+
         for (ColumnStruct oldStruct : oldStructList) {
-            if (oldStruct == null) {
-                continue;
-            }
-            String oldName = oldStruct.getColumnName();
-            if (oldName == null || oldName.length() <= 0) {
-                continue;
-            }
-            for (ColumnStruct newStruct : newStructList) {
-                if (newStruct == null) {
-                    continue;
-                }
-                if (compareColumnStruct(oldStruct, newStruct)) {
-                    return true;
-                }
+            if (existChangeColumn(newStructList, oldStruct)) {
+                return true;
             }
         }
         return false;
     }
 
     /**
-     * 比较列结构是否有变化
+     * 判断列结构集合中是否存在与给定列结构列名相同，且列的限制已改变的列
      *
-     * @param oldStruct
-     * @param newStruct
-     * @return
+     * @param newStructList 列结构集合
+     * @param oldStruct     给定列
+     * @return true：存在满足条件的列；false：不存在满足条件的列
      */
-    private static boolean compareColumnStruct(ColumnStruct oldStruct, ColumnStruct newStruct) {
+    private static boolean existChangeColumn(List<ColumnStruct> newStructList, ColumnStruct oldStruct) {
+        if (oldStruct == null) {
+            return false;
+        }
+        String oldName = oldStruct.getColumnName();
+        if (TextUtils.isEmpty(oldName)) {
+            // 列名异常，当做不满足条件处理
+            return false;
+        }
+        for (ColumnStruct newStruct : newStructList) {
+            if (isChangeColumnStruct(oldStruct, newStruct)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 比较列结构之间是否有变化
+     *
+     * @param oldStruct 比较列
+     * @param newStruct 被比较列
+     * @return true：改变；false：未改变
+     */
+    private static boolean isChangeColumnStruct(ColumnStruct oldStruct, ColumnStruct newStruct) {
         if (oldStruct == null || newStruct == null) {
             return false;
         }
@@ -243,67 +256,30 @@ public class DatabaseUtil {
         if (oldLimit == null && newLimit == null) {
             // 都没有限制符
             return false;
-        } else if (oldLimit == null || newLimit == null) {
-            // 某一个添加了限制符
-            System.out.println("某一个添加了限制符");
-            return true;
         } else {
-            if (!oldLimit.equals(newLimit)) {
-                // 修改了限制符
-                System.out.println("修改了限制符");
-                System.out.println("oldStruct = " + oldStruct.toString());
-                System.out.println("newStruct = " + newStruct.toString());
-                return true;
-            }
+            // 某一个添加了限制符 或者限制符被改变
+            return oldLimit == null || newLimit == null || !oldLimit.equals(newLimit);
         }
-        return false;
     }
 
     /**
-     * 添加自增的列到集合
+     * 获得数据表中已删除字段
      *
-     * @param columns
-     * @param oldStruct
-     * @return
-     */
-    public static List<String> addGeneratedId(List<String> columns, List<ColumnStruct> oldStruct) {
-        if (columns == null || oldStruct == null) {
-            return null;
-        }
-
-        for (ColumnStruct struct : oldStruct) {
-            // 自增的列不纳入数据拷贝的范围
-            if (struct != null && struct.getColumnLimit().contains("INTEGER PRIMARY KEY AUTOINCREMENT")) {
-                String columnName = struct.getColumnName();
-                if (!CollectionUtil.existValue(columnName, columns)) {
-                    columns.add(columnName);
-                }
-            }
-        }
-        return columns;
-    }
-
-    /**
-     * 获得数据表的增减字段
-     *
-     * @param oldColumns
-     * @param newColumns
-     * @return
+     * @param oldColumns 旧的表列结构集合
+     * @param newColumns 新的表列结构集合
+     * @return 已删除字段集合
      */
     public static List<String> getDeleteColumns(List<String> oldColumns, List<String> newColumns) {
-        if (oldColumns == null || newColumns == null) {
-            return null;
-        }
         return getColumnChange(oldColumns, newColumns);
     }
 
-    public static List<String> getAddColumns(List<String> oldColumns, List<String> newColumns) {
-        if (oldColumns == null || newColumns == null) {
-            return null;
-        }
-        return getColumnChange(newColumns, oldColumns);
-    }
-
+    /**
+     * 获取已更改的列集合
+     *
+     * @param oldColumns 旧的表列结构集合
+     * @param newColumns 新的表列结构集合
+     * @return 返回oldColumns中存在，而newColumns中不存在的列集合
+     */
     private static List<String> getColumnChange(List<String> oldColumns, List<String> newColumns) {
         List<String> columnList = new ArrayList<>();
         if (oldColumns == null || newColumns == null) {
@@ -347,3 +323,4 @@ public class DatabaseUtil {
     }
 
 }
+
